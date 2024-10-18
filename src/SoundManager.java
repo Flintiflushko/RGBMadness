b6196ceff091efb806f438c0e795e9c6faaef7c5
Flintiflushko <viktor.stefchev@gmail.com>
2024-10-19 01:04:07 +0200

    Added sfx upon attacks and game over.

---
+++ src/SoundManager.java
@@ -0,0 +1,29 @@
+
+import java.io.File;
+import javax.sound.sampled.*;
+
+public class SoundManager {
+
+    public void playGameOverSFX() {
+        try {
+            File sfx = new File("src/assets/sfx/GameOver.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(sfx);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (Exception e) {
+        }
+    }
+
+    public void playAttackSFX() {
+        try {
+            File sfx = new File("src/assets/sfx/Attack.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(sfx);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (Exception e) {
+        }
+    }
+    
+}
